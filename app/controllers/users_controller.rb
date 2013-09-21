70a72b39cab753c5d9a1c4ee2ec4b8e96fe9d184
Scott Ringwelski <sgringwe@mtu.edu>
2013-09-21 03:01:27 -0400

    Added create user files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 def UsersController
   def create
     # create a user
-    user = User.new(:username => "adelevie") user.password = "asecretpassword" user.save #=> true
+    user = User.new(:username => params[:username]) 
+    user.password = params[:password]
+    user.save #=> true
     # # after saving, the password is automatically hashed by Parse's server user.password will return the unhashed password when the original 
     # # object is in memory from a new session, User.where(:username => "adelevie").first.password will return nil check if a user is logged in
-    # User.authenticate("adelevie", "foooo") #=> false
-    # User.authenticate("adelevie", "asecretpassword") #=> #<User...>
   end
 end
