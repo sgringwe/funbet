1a9728104a5a71e71ef87c8cb9e9d46955a93d19
Scott Ringwelski <sgringwe@mtu.edu>
2013-09-21 00:33:38 -0400

    MHACKSSSSSSSSSSSSSSSS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funbet::Application.config.secret_token = 'e1e6aef768f444e641d74f6ee19d773c4201940d312f83766e7a36311b7cc1463752089d02ab6f046b58253660f040598626614f3d645f4b9eb73ae2dc0eeef2'
